0b0c63286340659d5646c6f834c644521fa2d197
Om Warkri <164020262+omwarkri@users.noreply.github.com>
2025-12-02 20:32:31 +0530

    Change database connection details in db.php
    Updated database credentials for development environment.

--- db.php
+++ db.php
@@ -1,18 +1,11 @@
 <?php
 $servername = "localhost";
-$username = "root"; // Change for live server
-<<<<<<< HEAD
-$password = "Radhakrushn1234";
-$database = "registration-db"; // Change for live server    
-=======
-$password = "Radhakrushn@1234";
-$database = "user_auth";
->>>>>>> 184ab10acb6fe9d1bc6901c94061ce4e9f549efb
+$username   = "phpuser";
+$password   = "StrongPass@123";
+$database   = "user_auth";
 
-// Create connection
 $conn = new mysqli($servername, $username, $password, $database);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
