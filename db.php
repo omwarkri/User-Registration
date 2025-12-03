5abfebec82b686dda90682490e89d72e1177be2d
omwarkri <omwarkri710@gmail.com>
2025-12-03 16:05:36 +0000

    add some changes in login and db files

--- db.php
+++ db.php
@@ -11,12 +11,17 @@ mysqli_options($conn, MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, false);
 
 // Connect to MySQL container
 $conn->real_connect(
-    "db",               // Docker service name
+    "db",               // Docker service name (from docker-compose.yml)
     "php-user",         // MySQL username
     "StrongPass@123",   // MySQL password
     "user_register",    // Database name
     3306                // MySQL port inside container
 );
 
-// Optional: set charset
+// Set charset
 $conn->set_charset("utf8mb4");
+
+// Optional: test connection (can comment out after working)
+// if ($conn->connect_errno) {
+//     die("Failed to connect to MySQL: " . $conn->connect_error);
+// }
